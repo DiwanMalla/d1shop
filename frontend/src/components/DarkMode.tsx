4aeb9135a451ce6b0b2ebb2d3bdbbba431a4bb82
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-03-13 17:46:03 +1100

    Changes the logical expression in the useEffect, theme section

--- frontend/src/components/DarkMode.tsx
+++ frontend/src/components/DarkMode.tsx
@@ -12,9 +12,7 @@ const DarkMode = () => {
     localStorage.setItem("theme", theme);
     if (theme == "dark") {
       element.classList.add("dark");
-      element.classList.add("dark");
     } else {
-      element.classList.remove("light");
       element.classList.remove("dark");
     }
   }, [theme]);
