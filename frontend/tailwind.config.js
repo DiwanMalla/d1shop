f7999c0e11adfe6307ecbdb3e1859ffe4f80fdd2
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-03-16 12:26:05 +1100

    Added category section

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
       colors: {
         primary: "#f42c37",
         secondary: "#f42c37",
-        brandYellow: "fdc62e",
+        brandYellow: "#fdc62e",
         brandGreen: "#2dcc67",
         brandBlue: "#1376f4",
         brandWhite: "#eeeeee",
